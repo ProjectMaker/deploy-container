b72ad5f9d9c02646993f9f5edd0127e6a72ce16a
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-01-11 09:39:43 +0100

    Fix imports

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restplus import Api
-from mongoengine import connect
 from werkzeug.contrib.fixers import ProxyFix
 
 from config import get_config
@@ -20,6 +19,5 @@ api = Api(app, version='2.0', title='Simulateur de devis')
 api.add_namespace(ns_webhook, path='/api/webhook')
 
 if __name__ == "__main__":
-    connect('uniroulotte', host='mongodb://root:Rudeboy788@ds137605.mlab.com:37605/uniroulotte', retryWrites=False)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
