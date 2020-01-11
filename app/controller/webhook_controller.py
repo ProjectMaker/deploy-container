a255388d32bdf420d916dc1d5dd46118c88000a8
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-01-11 17:21:13 +0100

    Check signature

--- app/controller/webhook_controller.py
+++ app/controller/webhook_controller.py
@@ -1,11 +1,13 @@
 from flask import request
 from flask_restplus import Namespace, Resource
 
+from services.auth_service import verify_hook
 ns = Namespace('webhook', description='USER Operations')
 
 @ns.route('/')
 class WebhookController(Resource):
     def post(self):
-        print(request.get_json())
+        print(verify_hook())
+        # print(request.get_json())
 
         return {}
---
+++ app/services/auth_service.py
@@ -0,0 +1,33 @@
+import hashlib
+import hmac
+import six
+from flask import abort, request
+
+
+def _get_header(key):
+    """Return message header"""
+
+    try:
+        return request.headers[key]
+    except KeyError:
+        print('Missing header ' + key)
+        abort(400, "Missing header: " + key)
+
+
+def _get_digest(secret):
+    return hmac.new(secret.encode(), request.data, hashlib.sha1).hexdigest()
+
+
+def verify_hook():
+    digest = _get_digest('Loosely21Burp32Lathrop')
+    sig_parts = _get_header("X-Hub-Signature").split("=", 1)
+    if not isinstance(digest, six.text_type):
+        digest = six.text_type(digest)
+
+    if len(sig_parts) < 2 or sig_parts[0] != "sha1" or not hmac.compare_digest(sig_parts[1], digest):
+        print('invalid signature')
+        abort(400, "Invalid signature")
+
+    return True
+
+
