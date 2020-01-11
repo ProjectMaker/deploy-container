e0b5661e6f0710fb7867cf35bf45443455e77eb4
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-01-11 18:37:15 +0100

    Execute ansible

--- app/controller/webhook_controller.py
+++ app/controller/webhook_controller.py
@@ -2,12 +2,13 @@ from flask import request
 from flask_restplus import Namespace, Resource
 from flask import current_app as app
 from services.auth_service import verify_hook
+from subprocess import call
 ns = Namespace('webhook', description='USER Operations')
 
 @ns.route('/')
 class WebhookController(Resource):
     def post(self):
-        print(verify_hook(app.logger))
-        # print(request.get_json())
+        if verify_hook(app.logger):
+            call('cd /home/uniroulotte/ansible && ./deploy_api_uniroulotte.sh')
 
         return {}
