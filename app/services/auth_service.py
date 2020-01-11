65c6332e85ea1ae898723091e8907a0995f42bf6
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-01-11 17:37:29 +0100

    Add some log

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -4,13 +4,13 @@ import six
 from flask import abort, request
 
 
-def _get_header(key):
+def _get_header(key, logger):
     """Return message header"""
-
+    logger.info('_get_header : ' + key)
     try:
         return request.headers[key]
     except KeyError:
-        print('Missing header ' + key)
+        logger.info('Missing header ' + key)
         abort(400, "Missing header: " + key)
 
 
@@ -18,14 +18,14 @@ def _get_digest(secret):
     return hmac.new(secret.encode(), request.data, hashlib.sha1).hexdigest()
 
 
-def verify_hook():
+def verify_hook(logger):
     digest = _get_digest('Loosely21Burp32Lathrop')
-    sig_parts = _get_header("X-Hub-Signature").split("=", 1)
+    sig_parts = _get_header("X-Hub-Signature", logger).split("=", 1)
     if not isinstance(digest, six.text_type):
         digest = six.text_type(digest)
 
     if len(sig_parts) < 2 or sig_parts[0] != "sha1" or not hmac.compare_digest(sig_parts[1], digest):
-        print('invalid signature')
+        logger.info('invalid signature')
         abort(400, "Invalid signature")
 
     return True
